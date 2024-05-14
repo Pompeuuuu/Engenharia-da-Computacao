8d49e880a8547035b8c9ac8ad0eace4c4a24afdc
Pedro Pompeu <141966855+Pompeuuuu@users.noreply.github.com>
2024-05-14 18:17:02 -0300

    Update staygood.sql

--- StayGood/staygood.sql
+++ StayGood/staygood.sql
@@ -1,159 +1,21 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 20/03/2024 às 00:04
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `staygood`
---
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `estoque`
---
-
-CREATE TABLE `estoque` (
-  `idEstoque` int(11) NOT NULL,
-  `quantidade` varchar(11) DEFAULT NULL,
-  `fkMedicamento` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `fornecedor`
---
-
-CREATE TABLE `fornecedor` (
-  `idFornecedor` int(11) NOT NULL,
-  `CNPJ` varchar(11) DEFAULT NULL,
-  `nomeFornecedor` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `funcionario`
---
-
-CREATE TABLE `funcionario` (
-  `idFuncionario` int(11) NOT NULL,
-  `nomeFuncionario` varchar(50) DEFAULT NULL,
-  `CPF` varchar(11) DEFAULT NULL,
-  `turno` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura para tabela `medicamento`
---
-
-CREATE TABLE `medicamento` (
-  `idMedicamento` int(11) NOT NULL,
-  `nomeMedicamento` varchar(50) DEFAULT NULL,
-  `CID` varchar(3) DEFAULT NULL,
-  `validade` date DEFAULT NULL,
-  `fkFornecedor` int(11) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Despejando dados para a tabela `medicamento`
---
-
-INSERT INTO `medicamento` (`idMedicamento`, `nomeMedicamento`, `CID`, `validade`, `fkFornecedor`) VALUES
-(3, 'TESTE', '1A2', '0000-00-00', NULL);
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices de tabela `estoque`
---
-ALTER TABLE `estoque`
-  ADD PRIMARY KEY (`idEstoque`),
-  ADD KEY `fkMedicamento` (`fkMedicamento`);
-
---
--- Índices de tabela `fornecedor`
---
-ALTER TABLE `fornecedor`
-  ADD PRIMARY KEY (`idFornecedor`);
-
---
--- Índices de tabela `funcionario`
---
-ALTER TABLE `funcionario`
-  ADD PRIMARY KEY (`idFuncionario`);
-
---
--- Índices de tabela `medicamento`
---
-ALTER TABLE `medicamento`
-  ADD PRIMARY KEY (`idMedicamento`),
-  ADD KEY `fkFornecedor` (`fkFornecedor`);
-
---
--- AUTO_INCREMENT para tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `estoque`
---
-ALTER TABLE `estoque`
-  MODIFY `idEstoque` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `fornecedor`
---
-ALTER TABLE `fornecedor`
-  MODIFY `idFornecedor` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `funcionario`
---
-ALTER TABLE `funcionario`
-  MODIFY `idFuncionario` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `medicamento`
---
-ALTER TABLE `medicamento`
-  MODIFY `idMedicamento` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- Restrições para tabelas despejadas
---
-
---
--- Restrições para tabelas `estoque`
---
-ALTER TABLE `estoque`
-  ADD CONSTRAINT `fkMedicamento` FOREIGN KEY (`fkMedicamento`) REFERENCES `medicamento` (`idMedicamento`);
-
---
--- Restrições para tabelas `medicamento`
---
-ALTER TABLE `medicamento`
-  ADD CONSTRAINT `fkFornecedor` FOREIGN KEY (`fkFornecedor`) REFERENCES `fornecedor` (`idFornecedor`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE estoque (
+  idEstoque primary key not null,
+  quantidade varchar(11) NOT NULL,
+);
+CREATE TABLE fornecedor (
+  idFornecedor primary key not null,
+  CNPJ varchar(11) not null,
+  nomeFornecedor varchar(50) not null
+);
+CREATE TABLE funcionario (
+  idFuncionario primary key not null,
+  nomeFuncionario varchar(50) not null,
+  CPF varchar(11) not null,
+  turno varchar(50) not null
+);
+CREATE TABLE medicamento (
+  idMedicamento primary key not null,
+  nomeMedicamento varchar(50) not null,
+  CID` varchar(3) not null,
+  validade` date not nullL
+)
